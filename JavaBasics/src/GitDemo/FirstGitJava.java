607f8ccf1db16f2d77ce6810205ded320b8aa444
adityabisht <aditya_bisht@hotmail.com>
2017-09-17 15:50:19 -0500

    First Commit in GIT

---
+++ JavaBasics/src/GitDemo/FirstGitJava.java
@@ -0,0 +1,10 @@
+package GitDemo;
+
+public class FirstGitJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("FirstLine To GitHub with Jenkins - Version 2");
+	}
+
+}
