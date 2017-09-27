e42db661ccee56aa561caa9f4c0d12ce65b76a6f
adityabisht <aditya_bisht@hotmail.com>
2017-09-26 23:29:14 -0500

    Formatted

--- JavaBasics/src/GitDemo/gcd.java
+++ JavaBasics/src/GitDemo/gcd.java
@@ -6,29 +6,29 @@ public class gcd {
 public static void main(String [] args){
 	
 	/*int a= 125;
-	int b= 2525;*/
+int b= 2525;*/
+
+try (Scanner input = new Scanner(System.in)){
 	
-	try (Scanner input = new Scanner(System.in)){
+System.out.print("Enter the first digit : ");	
+int a = input.nextInt();
+
+System.out.print("Enter the second digit : ");	
+int b = input.nextInt();
+
+int gcd=1;
+for(int i = 2;i<a && i<b;i++){
+	
+	if(a%i==0 && b%i==0){
 		
-	System.out.print("Enter the first digit : ");	
-	int a = input.nextInt();
+		gcd = i;
+	}//End of If
 	
-	System.out.print("Enter the second digit : ");	
-	int b = input.nextInt();
-	
-	int gcd=1;
-	for(int i = 2;i<a && i<b;i++){
-		
-		if(a%i==0 && b%i==0){
-			
-			gcd = i;
-		}//End of If
-		
-	}//End of for loop
-	
-	System.out.println("GCD is : "+gcd);
-	
-	}//End of Try
+}//End of for loop
+
+System.out.println("GCD is : "+gcd);
+
+}//End of Try
 	
 }//End of Main Method
 }//End of Class
